edb1fdea184c2047327c51189563e45191180cc0
Yoo <yyk7305@naver.com>
2025-07-01 21:46:53 +0900

    YHG_AbilitySuccess!
    어빌리티 발동 성공!

--- Source/WutheringWaves/Private/YHG/AbilitySystem/Abilities/PlayerGameplayAbility.cpp
+++ Source/WutheringWaves/Private/YHG/AbilitySystem/Abilities/PlayerGameplayAbility.cpp
@@ -1,10 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "YHG/AbilitySystem/Abilities/PlayerGameplayAbility.h"
-
 #include "Common/WWGameplayTags.h"
 #include "Common/AbilitySystem/WWAbilitySystemComponent.h"
+#include "Common/PlayerControllers/WWPlayerController.h"
 #include "YHG/PlayerCharacters/PlayerCharacter.h"
 
 APlayerCharacter* UPlayerGameplayAbility::GetPlayerCharacterFromActorInfo()
@@ -17,11 +16,11 @@ APlayerCharacter* UPlayerGameplayAbility::GetPlayerCharacterFromActorInfo()
 	return CachedPlayerCharacter.IsValid() ? CachedPlayerCharacter.Get() : nullptr;
 }
 
-APlayerController* UPlayerGameplayAbility::GetPlayerControllerFromActorInfo()
+AWWPlayerController* UPlayerGameplayAbility::GetPlayerControllerFromActorInfo()
 {
 	if (!CachedPlayerController.IsValid())
 	{
-		CachedPlayerController = Cast<APlayerController>(CurrentActorInfo->PlayerController);
+		CachedPlayerController = Cast<AWWPlayerController>(CurrentActorInfo->PlayerController);
 	}
 
 	return CachedPlayerController.IsValid() ? CachedPlayerController.Get() : nullptr;
