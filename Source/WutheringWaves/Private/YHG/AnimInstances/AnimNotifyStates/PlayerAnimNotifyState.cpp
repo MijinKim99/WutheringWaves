2c97223566bdc6483cd0451f6462a70f6a67b7bf
MijinKim99 <108381780+MijinKim99@users.noreply.github.com>
2025-07-14 20:45:20 +0900

    KMJ_0714_FinalMerge

--- Source/WutheringWaves/Private/YHG/AnimInstances/AnimNotifyStates/PlayerAnimNotifyState.cpp
+++ Source/WutheringWaves/Private/YHG/AnimInstances/AnimNotifyStates/PlayerAnimNotifyState.cpp
@@ -5,18 +5,35 @@
 
 #include "Common/PlayerControllers/WWPlayerController.h"
 #include "Common/WWDebugHelper.h"
+#include "YHG/PlayerCharacters/PlayerCharacter.h"
 
 void UPlayerAnimNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
                                          float TotalDuration, const FAnimNotifyEventReference& EventReference)
 {
 	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
 
-	AController* Controller = MeshComp->GetOwner()->GetInstigatorController();
+	SkeletalMeshComponent = MeshComp;
+
+	if (!SkeletalMeshComponent)
+	{
+		Debug::Print(TEXT("PlayerAnimNotifyState : Can't find SkeletalMeshComponent"));
+		return;
+	}
+
+	
+	PlayerCharacter = Cast<APlayerCharacter>(MeshComp->GetOwner());
+
+	if (!PlayerCharacter)
+	{
+		Debug::Print(TEXT("PlayerAnimNotifyState : Failed Cast PlayerCharacter"));
+		return;
+	}
+	
 	WWPlayerController = Cast<AWWPlayerController>(MeshComp->GetOwner()->GetInstigatorController());
 
 	if (!WWPlayerController)
 	{
-		Debug::Print(TEXT("CancelAbilityByNative : Failed Cast WWPlayerController"));
+		Debug::Print(TEXT("PlayerAnimNotifyState : Failed Cast WWPlayerController"));
 		return;
 	}
 }
